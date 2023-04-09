e34a8920266490a10feb4f38079017de5308e530
Gustavo <gustavo_r69@yahoo.com.ar>
2023-04-09 17:56:44 -0300

    ejercicio de variables martes 4 de abril de 2023

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+//declaramos e inicializamos
+let letraInicialPais= "a";
+let chatMsjReunion = " hola";
+let estadoServicio = true;
+let pesoBotellalitro = 1;
+let nombCentroFormacion = "cfp27";
+let contraseniaYoutube = 'alk1124_:"$1_".';
+let celularNumero= "1155151585";
+let nombPostre= "selva negra";
+let claveProducto= "123U-23J-3232J";
